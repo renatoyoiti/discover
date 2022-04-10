236eb4f502e7c718165c3066144a0c46e84174f0
Renato Yoiti <renatoyoiti@gmail.com>
2022-04-10 12:39:11 -0300

    Save

---
+++ starter-js/functions.js
@@ -0,0 +1,9 @@
+// function statement
+function createPhrases () {
+  console.log('Study is very good')
+  console.log('Patience and persistence')
+  console.log('Review is the mother of knowledge')
+}
+
+// execute, run, call, invoke
+createPhrases()
---
+++ starter-js/script.js
@@ -0,0 +1,20 @@
+let weight
+console.log(typeof weight)
+
+let name = "Renato Yoiti"
+let age = 23
+let stars = 4.8
+let isSubscribed = true
+
+let student = {
+  name: "Giovanna",
+  age: 19,
+  weight: 49.5
+}
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`)
+
+let students = []
+
+students.push(student)
+console.log(students[0])
